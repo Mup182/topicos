d66d3b48949de8e2395027b02b6519144f65dc7a
Mup182 <murilomposser@gmail.com>
2025-10-13 16:40:50 -0300

    aula2

--- telas/tela_menu.py
+++ telas/tela_menu.py
@@ -3,6 +3,33 @@ import arcade
 class TelaMenu(arcade.View):
     def __init__(self):
         super().__init__()
+        self.background_color = arcade.color.WHITE
+        self.spriteList = arcade.SpriteList()
+
+
+        self.background_image = arcade.Sprite("./asets/imagens/download.png",
+                                             
+                                              center_x=self.window.width // 2,
+                                              center_y=self.window.width // 2
+                                              )
+        
+        self.spriteList.append(self.background_image)
+        #self.botao1 = arcade.Sprite(" ./asets/imagens/retangulo.png")
+        #self.spriteList.append(self.botao1)
+
+        def on_draw(self):
+            self.clear()
+            self.spriteList.draw()
+        
+
+
+
+
+
+
+'''class TelaMenu(arcade.View):
+    def __init__(self):
+        super().__init__()
         self.background_color = arcade.color.CORAL
         self.background_image = arcade.texture("./asets/imagens/fundo_menu.png")
         
@@ -12,4 +39,5 @@ class TelaMenu(arcade.View):
        arcade.draw_texture_rect(
            self.background_image,
            arcade.XYWH(540, 120, self.background_image.width, self.background_image.height).scale(0.2)
-       )
+       )'''
+       
